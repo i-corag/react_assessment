db8e74939ebf621b33cb26f0d93611b444013b13
i-corag <icoraglio@gmail.com>
2023-06-12 13:42:13 +0200

    add footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import './footer.css'
+
+const Footer = () => {
+
+    return (
+        <footer>
+            <p><span>©</span> Copyright Social Brothers - 2023</p>
+        </footer>
+    )
+}
+
+export default Footer
